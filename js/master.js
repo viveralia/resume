fc948574bff051afbc5c470f068e8f0e1dd9b8d5
viveralia <edgarv10@gmail.com>
2018-11-07 15:15:59 -0600

    First Commit

---
+++ js/master.js
@@ -0,0 +1,61 @@
+// Opacity transition
+var timelineItem = document.getElementsByClassName('timeline-item')
+
+var waypoint = new Waypoint({
+  element: timelineItem[0],
+  handler: function () {
+    timelineItem[0].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
+
+var waypoint = new Waypoint({
+  element: timelineItem[1],
+  handler: function () {
+    timelineItem[1].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
+
+var waypoint = new Waypoint({
+  element: timelineItem[2],
+  handler: function () {
+    timelineItem[2].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
+var waypoint = new Waypoint({
+  element: timelineItem[3],
+  handler: function () {
+    timelineItem[3].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
+var waypoint = new Waypoint({
+  element: timelineItem[4],
+  handler: function () {
+    timelineItem[4].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
+var waypoint = new Waypoint({
+  element: timelineItem[5],
+  handler: function () {
+    timelineItem[5].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
+var waypoint = new Waypoint({
+  element: timelineItem[6],
+  handler: function () {
+    timelineItem[6].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
+var waypoint = new Waypoint({
+  element: timelineItem[7],
+  handler: function () {
+    timelineItem[7].classList.toggle('opacity-reveal')
+  },
+  offset: 350
+})
